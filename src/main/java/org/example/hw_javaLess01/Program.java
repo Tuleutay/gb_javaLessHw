ae00ade0a03e3ae7ed4ea404ce237ef0bcc80671
Tuleutay <you@example.com>
2023-05-16 10:53:34 +0600

    2 commit

--- src/main/java/org/example/hw_javaLess01/Program.java
+++ src/main/java/org/example/hw_javaLess01/Program.java
@@ -42,27 +42,27 @@ public class Program {
 
         // #region Teams
 
-//        int teamCount = 10;
-//        Random rand = new Random();
-//        int magicianCount = 0;
-//        int priestCount = 0;
-//
-//
-//        List<BaseHero> teams = new ArrayList<>();
-//        for (int i = 0; i < teamCount; i++) {
-//            if (rand.nextInt(2) == 0) {
-//                teams.add(new Priest());
-//                priestCount++;
-//            }
-//            else{
-//                teams.add(new Magician());
-//                magicianCount++;
-//            }
-//
-//            System.out.println(teams.get(i).getInfo());
-//        }
-//        System.out.println();
-//        System.out.printf("magicalCount: %d priestCount: %d \n\n", magicianCount, priestCount);
+        int teamCount = 10;
+        Random rand = new Random();
+        int magicianCount = 0;
+        int priestCount = 0;
+
+
+        List<BaseHero> teams = new ArrayList<>();
+        for (int i = 0; i < teamCount; i++) {
+            if (rand.nextInt(2) == 0) {
+                teams.add(new Priest());
+                priestCount++;
+            }
+            else{
+                teams.add(new Magician());
+                magicianCount++;
+            }
+
+            System.out.println(teams.get(i).getInfo());
+        }
+        System.out.println();
+        System.out.printf("magicalCount: %d priestCount: %d \n\n", magicianCount, priestCount);
 
         // attack
 
